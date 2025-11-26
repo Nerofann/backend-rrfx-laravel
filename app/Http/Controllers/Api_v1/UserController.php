f770faa7731dfcec714bc379a0fe23a73ffa03cd
Fani Annur Rozi <crazyhx4@gmail.com>
2025-11-26 17:11:39 +0700

    Add UserController with userinfo method for user data retrieval

---
+++ app/Http/Controllers/Api_v1/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api_v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Support\ApiResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function userinfo() {
+        $user = (object) auth()->user();
+        return ApiResponse::success("OK", [
+            'fullname' => $user->name,
+            'email' => $user->email,
+            'username' => $user->username,
+            'created_at' => $user->created_at,
+            'user_type' => $user->user_type,
+            'place_of_birth' => $user->place_of_birth,
+            'date_of_birth' => $user->date_of_birth,
+            'address' => $user->address,
+            'country_id' => $user->country_id,
+            'phone' => $user->phone,
+            'phone_code' => $user->phone_code,
+            'is_verified' => $user->is_verified,
+            'app_theme' => $user->app_theme,
+        ]);
+    }
+}
