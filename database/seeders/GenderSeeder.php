8f792db3b44afd07cfd8f31916552b93c4a2f0ad
Fani Annur Rozi <crazyhx4@gmail.com>
2025-12-14 08:23:38 +0700

    membuat file seeder untuk table genders

---
+++ database/seeders/GenderSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserGender;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GenderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table("genders")->insertOrIgnore([
+            [
+                'name' => "Laki-laki",
+                'created_at' => date("Y-m-d H:i:s") 
+            ],
+            [
+                'name' => "Perempuan",
+                'created_at' => date("Y-m-d H:i:s") 
+            ]
+        ]);
+    }
+}
