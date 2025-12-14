928ededd7cec9440e0c7dc1d2d81a8c623502ac0
Fani Annur Rozi <crazyhx4@gmail.com>
2025-12-14 13:34:02 +0700

    menambahkan relasi di model user ke model gender

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,4 +64,9 @@ class User extends Authenticatable implements JWTSubject
         return [];
     }
 
+
+    public function gender()
+    {
+        return $this->belongsto(Gender::class, 'gender_id');
+    }
 }
